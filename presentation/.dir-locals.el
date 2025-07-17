81f07acf08e58e8d3dd745a140f7c24e5c26fa6d
pang <psdpfw+pang@gmail.com>
2025-07-17 10:15:07 +0800

    update

--- presentation/.dir-locals.el
+++
@@ -1,7 +0,0 @@
-(;; Dir settings
- ("" . ((org-mode . ((eval . (progn
-				      ;; Read content of revealjs-plugins-conf and set assign it to org-re-reveal-init-script
-				      (setq org-re-reveal-init-script
-					    (with-temp-buffer
-					      (insert-file-contents "./revealjs-plugins-conf.js")
-					      (buffer-string))))))))))
